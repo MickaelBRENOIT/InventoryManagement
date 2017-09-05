9dd59fed804afc7cf5521218805576066c5859cf
Mickael <brenoit.mickael@gmail.com>
2017-09-05 21:53:37 +0200

    Added the Log In view

---
+++ src/com/mickaelbrenoit/inventorymanagement/ControllerLogIn.java
@@ -0,0 +1,4 @@
+package com.mickaelbrenoit.inventorymanagement;
+
+public class ControllerLogIn {
+}
--- src/com/mickaelbrenoit/inventorymanagement/Main.java
+++ src/com/mickaelbrenoit/inventorymanagement/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.mickaelbrenoit.inventorymanagement;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("loginview.fxml"));
+        primaryStage.setTitle("Log In");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
